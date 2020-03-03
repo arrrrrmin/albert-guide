a7dc6c1cee65895df87dcb9711f1930e464e55b6
arrrrrmin <info@dotarmin.info>
2020-03-03 18:22:57 +0100

    Update on data script

--- wikidata/we_need_data.sh
+++ wikidata/we_need_data.sh
@@ -11,9 +11,6 @@ echo "Downloading $wikidata_dump ..."
 
 extracted="extracted/"
 
-# WikiExtractor.py --min_text_length --no-templates --json
-
-
 if [ -f $wikidata_dump ]; then
   cd ../wikiextractor
   python3 WikiExtractor.py  -b 55000K --section --min_text_length 300 --no_templates --json \
