702e912d93400da54693b833adcaaa2ed2eb9288
arrrrrmin <info@dotarmin.info>
2020-03-05 17:46:42 +0100

    Data dir

--- wikidata/we_need_data.sh
+++ wikidata/we_need_data.sh
@@ -9,15 +9,15 @@ langcode=$1
 wikidata_dump="${langcode}wiki-latest-pages-articles.xml.bz2"
 echo "Downloading $wikidata_dump ..."
 
-extracted="extracted/"
+data="data/"
 
 if [ -f $wikidata_dump ]; then
   cd ../wikiextractor
   python3 WikiExtractor.py  -b 55000K --section --min_text_length 300 --no_templates --json \
-                            -o "../wikidata/${extracted}" "../wikidata/${wikidata_dump}"
+                            -o "../wikidata/${data}" "../wikidata/${wikidata_dump}"
 else
   wget "http://download.wikimedia.org/${langcode}wiki/latest/${wikidata_dump}"
   cd ../wikiextractor
   python3 WikiExtractor.py  -b 55000K --section --min_text_length 300 --no_templates --json \
-                            -o "../wikidata/${extracted}" "../wikidata/${wikidata_dump}"
+                            -o "../wikidata/${data}" "../wikidata/${wikidata_dump}"
 fi
